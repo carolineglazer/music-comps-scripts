55392a55fe05ea1e9a8890b7b26763dd608a4700
Caroline Glazer <carolineroseglazer@gmail.com>
2016-07-29 10:43:58 -0700

    python script to transpose and convert melody files to .xml files in F Major or f minor

---
+++ transpose_convert.py
@@ -0,0 +1,53 @@
+from music21 import *
+import os
+import re
+from colorama import Fore
+
+for f in sorted(os.listdir('../sources/music/')):
+    if f.endswith('.xml'):
+        peggy = converter.parse('../sources/music/'+f)
+        #print(peggy.metadata.title)
+        key = str(peggy.analyze('key'))
+        print(f)
+
+        #convert all major to F major and all minor to f minor
+        if key == 'F major':
+            print('yay already in F!')
+            peggy2 = peggy
+        elif key == 'a minor':
+            peggy2 = peggy.transpose(-4)
+            print(key, '==>', peggy2.analyze('key'))
+        elif key == 'G major':
+            peggy2 = peggy.transpose(-2)
+            print(key, '==>', peggy2.analyze('key'))
+        elif key == 'C major':
+            peggy2 = peggy.transpose(-7)
+            print(key, '==>',peggy2.analyze('key'))
+        elif key == 'e minor':
+            peggy2 = peggy.transpose(1)
+            print(key, '==>', peggy2.analyze('key'))
+        elif key == 'A major':
+            peggy2 = peggy.transpose(-4)
+            print(key, '==>', peggy2.analyze('key'))
+        else:
+            print(Fore.RED + "OH NO!:", f, key)
+            
+        #write a new xml file with the same name to .../music/xml_same_key
+        outfile = '../sources/music/xml_same_key/'+re.search('.*(?=.xml)', f).group()+'.xml'
+        if outfile not in os.listdir('../sources/music/xml_same_key/'):
+        peggy2.write(fmt='xml', fp=outfile)
+
+
+
+#################################
+'''
+for i in peggy[2].measures(0,len(peggy[2])):
+    print(i.offset)
+    pitches = {}
+    for thing in i.getElementsByClass('Note'):
+        pitches[thing.pitch] = thing.offset
+    for k,v in pitches.items():
+        print(k,v)
+'''
+
+
