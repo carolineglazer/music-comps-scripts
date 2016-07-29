ff92535597a81a6eaa9856f92944d112baaed349
Caroline Glazer <carolineroseglazer@gmail.com>
2016-07-29 10:51:39 -0700

    added verse_counter.py and song.py

---
+++ song.py
@@ -0,0 +1,11 @@
+class Variant:
+
+    def __init__(self, num=None, title=None, location=None, num_of_verses=None, verse_order=[], names=[], places=[], fragment=False):
+        self.num = num
+        self.title = title
+        self.location = location
+        self.num_of_verses = num_of_verses
+        self.verse_order = verse_order
+        self.names = names
+        self.places = places
+        self.fragment = fragment
---
+++ verse_counter.py
@@ -0,0 +1,16 @@
+import os
+
+for i in os.listdir('../sources/lyrics'):
+    variant = '../sources/lyrics/' + i
+    f = open(variant, 'r')
+    verses = -1
+    print 'start: ', verses
+
+    for line in f:
+        if line == '\n':
+            verses += 1
+            print variant, 'verse'
+
+    print variant, ": ", verses
+    f.close()
+    print 'end: ', verses
