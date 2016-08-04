1ad0c2530b398bad212924ab73070aec5629194b
Caroline Glazer <carolineroseglazer@gmail.com>
2016-08-04 11:19:55 -0700

    test print statments

--- add_metadata.py
+++ add_metadata.py
@@ -13,16 +13,21 @@ for f in sorted(os.listdir('../sources/music/')):
         print(f)
 
         #add a metadata object to peggy score
-        peggy.insert(0,metadata.Metadata())
+        #peggy.insert(0,metadata.Metadata())
         
         #add stuff to the metadata object
+        peggy.show('text')
         peggy.metadata.localeOfComposition = input("location: ")
         peggy.metadata.date = input("year: ")        
         peggy.metadata.number = key
         
         #overwrite xml files with metadata-y version
         outfile = '../sources/music/'+f
-        peggy.write(fmt='xml', fp=outfile)
-        print(peggy.metadata.all())
+        peggy2 = peggy
+        print(peggy2.metadata.all())
+        peggy2.write(fmt='xml', fp=outfile)
         peggytest = converter.parse('../sources/music/'+f)
         print(peggytest.metadata.all())
+        print(peggytest.metadata.localeOfComposition)
+        print(peggytest.metadata.date)
+        print(peggytest.metadata.number)
