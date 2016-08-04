1924a14ddb678df7040b958615780e1b78cfb290
Caroline Glazer <carolineroseglazer@gmail.com>
2016-08-04 10:09:55 -0700

    change metadata element names and insert Metadata object at start of score

--- add_metadata.py
+++ add_metadata.py
@@ -12,13 +12,20 @@ for f in sorted(os.listdir('../sources/music/')):
         key = str(peggy.analyze('key'))
         print(f)
 
-        peggy.metadata.alternativeTitle = input("title: ")
-        peggy.metadata.localeOfComposition = input("location: ")
-        peggy.metadata.number = key
-        peggy.metadata.date = input("year: ")        
+        #add a metadata object to peggy score
+        peggy.insert(0,metadata.Metadata())
         
-        print(peggy.metadata.all())
-
+        #add stuff to the metadata object
+        peggy.metadata.number = f
+        peggy.metadata.title = input("title: ")
+        peggy.metadata.location = input("location: ")
+        peggy.metadata.year = input("year: ")        
+        peggy.metadata.orig_key = key
+        peggy.metadata.meter = thisScore.recurse().getElementsByClass(meter.TimeSignature)[0]
+        
         #overwrite xml files with metadata-y version
         outfile = '../sources/music/'+f
         peggy.write(fmt='xml', fp=outfile)
+        print(peggy.metadata.all())
+        peggytest = converter.parse('../sources/music/'+f)
+        print(peggytest.metadata.all())
