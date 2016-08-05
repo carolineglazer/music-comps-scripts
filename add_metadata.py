a5ae5ee9676d8b5c04120d79b734015bc738f44b
Caroline Glazer <carolineroseglazer@gmail.com>
2016-08-04 17:26:25 -0700

    use unfortunate metadata for workaround

--- add_metadata.py
+++ add_metadata.py
@@ -1,29 +1,25 @@
 #Author: Caroline Glazer
-#Date: July-August 2016
+#Date: August 2016
 
 from music21 import *
 import os
-import re
-from colorama import Fore
+from copy import deepcopy
 
 for f in sorted(os.listdir('../sources/music/')):
     if f.endswith('.xml'):
         peggy = converter.parse('../sources/music/'+f)
         key = str(peggy.analyze('key'))
         print(f)
-
-        #add stuff to the metadata object
-        peggy.metadata.localeOfComposition = input("location: ")
-        peggy.metadata.date = input("year: ")        
-        peggy.metadata.number = key
+        peggy2 = deepcopy(peggy)
         
+        #add stuff to the metadata object
+        peggy2.metadata.composer = input("location: ")        
+        peggy2.metadata.movementNumber = input("year: ")
+
         #overwrite xml files with metadata-y version
         outfile = '../sources/music/'+f
-        peggy2 = peggy
-        print(peggy2.metadata.all())
+        print(outfile)
         peggy2.write(fmt='xml', fp=outfile)
-        peggytest = converter.parse('../sources/music/'+f)
+        print(peggy2.metadata.all())
+        peggytest = converter.parse(outfile)
         print(peggytest.metadata.all())
-        print(peggytest.metadata.localeOfComposition)
-        print(peggytest.metadata.date)
-        print(peggytest.metadata.number)
